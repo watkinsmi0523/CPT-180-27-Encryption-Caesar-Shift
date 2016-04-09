6dd280ab35cc08e109b65559d2b20312f4aa89f1
Paul Scarrone <paul.scarrone@gmail.com>
2016-04-09 13:43:06 -0400

    Conan setup and cmake configuration for gtest

---
+++ conanfile.py
@@ -0,0 +1,7 @@
+from conans import ConanFile, CMake
+
+class CaesarShift(ConanFile):
+   settings = "os", "compiler", "build_type", "arch"
+   requires = "gtest/1.7.0@lasote/stable"
+   generators = "cmake", "gcc", "txt"
+   #default_options = "Poco:shared=True", "OpenSSL:shared=False"
